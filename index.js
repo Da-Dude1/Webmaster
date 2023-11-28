bacfeadc4de90b9b3bf8b1a649e6a5c83dedb182
Da-Dude1 <72897796+Da-Dude1@users.noreply.github.com>
2023-11-27 23:13:02 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,47 @@
+let innerCursor = document.querySelector('.inner-cursor');
+let outerCursor = document.querySelector('.outer-cursor');
+let hamburger = document.querySelector('.hamburger');
+
+document.addEventListener('mousemove', moveCursor);
+
+function moveCursor(event){
+    let x = event.clientX;
+    let y = event.clientY;
+
+    innerCursor.style.left = `${x}px`;
+    innerCursor.style.top = `${y}px`;
+
+    outerCursor.style.left = `${x}px`;
+    outerCursor.style.top = `${y}px`;
+}
+
+let links = Array.from(document.querySelectorAll("a"));
+console.log(links)
+
+links.forEach(link => {
+    link.addEventListener("mouseover", () => {
+        innerCursor.classList.add("grow");
+    });
+    link.addEventListener("mouseleave", () => {
+        innerCursor.classList.remove("grow");
+    });
+})
+
+hamburger.addEventListener("click", () => {
+    let navbar = document.querySelector('.nav-link-container')
+    navbar.classList.toggle('active')
+})
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if(entry.isIntersecting){
+            entry.target.classList.add('show')
+        }
+        else{
+            entry.target.classList.remove('show')
+        }
+    })
+})
+
+const hiddenElements = document.querySelectorAll('.hidden')
+hiddenElements.forEach((el) => observer.observe(el))
